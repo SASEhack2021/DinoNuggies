af7c113a443b2e738018f8ee0a8d64557881e9d4
rylenMang0 <81496460+rylenMang0@users.noreply.github.com>
2021-03-28 00:50:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+from discord.ext import commands
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix="$")
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+@bot.command(name="remindme")
+
+client.run(os.getenv('TOKEN'))
